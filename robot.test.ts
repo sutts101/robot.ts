a20dd3434a7e126e487c0e94753607b00824e14a
Ian Sutton <sutts101@gmail.com>
2021-02-05 22:24:54 +0800

    grid: contains

--- robot.test.ts
+++ robot.test.ts
@@ -1,4 +1,4 @@
-import {Position} from './robot'
+import {Position, SquareGrid} from './robot'
 
 describe('Position', () => {
   describe('translate', () => {
@@ -9,6 +9,24 @@ describe('Position', () => {
       expect(position.translate(1, 1).toString()).toEqual('4,4')
       expect(position.translate(2, 3).toString()).toEqual('5,6')
       expect(position.translate(-100, -300).toString()).toEqual('-97,-297')
+    })
+  })
+})
+
+describe('SquareGrid', () => {
+  describe('contains', () => {
+    test('does what it says on the tin', () => {
+      expect(new SquareGrid(5).contains(new Position(0, 0))).toEqual(true)
+      expect(new SquareGrid(5).contains(new Position(2, 3))).toEqual(true)
+      expect(new SquareGrid(5).contains(new Position(4, 4))).toEqual(true)
+
+      expect(new SquareGrid(5).contains(new Position(-1, 0))).toEqual(false)
+      expect(new SquareGrid(5).contains(new Position(0, -1))).toEqual(false)
+      expect(new SquareGrid(5).contains(new Position(4, 5))).toEqual(false)
+      expect(new SquareGrid(5).contains(new Position(5, 4))).toEqual(false)
+
+      expect(new SquareGrid(10).contains(new Position(9, 9))).toEqual(true)
+      expect(new SquareGrid(10).contains(new Position(9, 10))).toEqual(false)
     })
   })
 })
--- robot.ts
+++ robot.ts
@@ -10,3 +10,15 @@ export class Position {
   translate = (x: number, y: number): Position => new Position(this.x + x, this.y + y)
   toString = () => `${this.x},${this.y}`
 }
+
+export class SquareGrid {
+  readonly size: number;
+
+  constructor(size: number) {
+    this.size = size;
+  }
+
+  contains(position: Position): boolean {
+    return position.x >= 0 && position.x < this.size && position.y >= 0 && position.y < this.size; 
+  }
+}
